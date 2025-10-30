730308037f31876295f152eb32239abe613aa15f
daniel <daniel15898@gmail.com>
2025-10-30 08:03:27 +0200

    clean app with basic providers

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,14 @@
+import { Container } from '@/components/Container';
 import { Stack, Link } from 'expo-router';
 
-import { View } from 'react-native';
-
-import { Button } from '@/components/Button';
-import { Container } from '@/components/Container';
-import { ScreenContent } from '@/components/ScreenContent';
+import { View, Text } from 'react-native';
 
 export default function Home() {
   return (
     <View className={styles.container}>
       <Stack.Screen options={{ title: 'Home' }} />
       <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
+        <Text>Hello World</Text>
       </Container>
     </View>
   );
