bce5b143ff9ba4724efc371345e8d82921e3c664
daniel <daniel15898@gmail.com>
2025-11-01 15:56:16 +0200

    import i18n for RTL initializtion

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,11 @@
 import NavThemeContext from '@/context/theme-context';
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import '@/global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 import { queryConfig } from '@/lib/react-query';
+import '@/i18n';
+import '@/global.css';
 
 export default function RootLayout() {
   const [queryClient] = useState(
