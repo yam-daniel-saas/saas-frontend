40ac4ffd3f208ed9a7cf07f645e573049f666b4c
daniel <daniel15898@gmail.com>
2025-11-29 14:34:37 +0200

    move files location

--- src/app/(auth)/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-  // For now, redirect to sign-in screen
-  // Later, you can add logic to check if user is authenticated
-  // and redirect to either sign-in or protected routes
-  return <Redirect href="/(auth)/sign-in" />;
-}
