a2de09f75a69eefcb8af457ef54a47b44453deeb
daniel <daniel15898@gmail.com>
2025-11-29 14:35:42 +0200

    application layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,31 +1,34 @@
-import NavThemeContext from '@/context/theme-context';
 import { Slot, SplashScreen, Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
-import { queryConfig } from '@/lib/react-query';
+import { queryConfig } from '@/services/react-query/query-config';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
+import NavThemeContext from '@/context/theme-context';
+import SplashScreenController from '@/components/SplashScreenController';
 import '@/i18n';
 import '@/global.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: queryConfig,
+});
+
 export default function RootLayout() {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: queryConfig,
-      })
-  );
+  //add ota update check - https://docs.expo.dev/updates/introduction/
+  //add sentry error reporting - https://docs.expo.dev/sentry/introduction/
+  //add font loading - https://docs.expo.dev/guides/using-custom-fonts/
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <KeyboardProvider>
+    <KeyboardProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
         <NavThemeContext>
           <QueryClientProvider client={queryClient}>
-            <RootNavigator />
+            <SplashScreenController>
+              <RootNavigator />
+            </SplashScreenController>
           </QueryClientProvider>
         </NavThemeContext>
-      </KeyboardProvider>
-    </GestureHandlerRootView>
+      </GestureHandlerRootView>
+    </KeyboardProvider>
   );
 }
 
